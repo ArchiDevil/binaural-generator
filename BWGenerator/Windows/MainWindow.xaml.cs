88fe6ecab7f3b9b725e6ad989142b7e9ff2d996b
ArchiDevil <archidevil52@gmail.com>
2015-10-23 15:06:27 +0300

    Moved windows into folder

--- BWGenerator/Windows/MainWindow.xaml.cs
+++ BWGenerator/Windows/MainWindow.xaml.cs
@@ -4,16 +4,44 @@ using System.Windows;
 using System.Windows.Controls;
 
 using BWGenerator.Models;
-using SharedContent.AudioProviders;
+
+using SharedLibrary.AudioProviders;
 
 using OxyPlot;
-using OxyPlot.Axes;
-using OxyPlot.Series;
 
 namespace BWGenerator
 {
     public partial class MainWindow : Window
     {
+        SignalPlotViewModel[] models = new SignalPlotViewModel[2];
+        NoisePlotViewModel noiseSmoothnessModel = null;
+        NoisePlotViewModel noiseVolumeModel = null;
+
+        public double CarrierSelector(SignalPoint point)
+        {
+            return point.CarrierValue;
+        }
+
+        public double DifferenceSelector(SignalPoint point)
+        {
+            return point.DifferenceValue;
+        }
+
+        public double VolumeSelector(SignalPoint point)
+        {
+            return point.VolumeValue;
+        }
+
+        public double NoiseSmoothnessSelector(NoisePoint point)
+        {
+            return point.SmoothnessValue;
+        }
+
+        public double NoiseVolumeSelector(NoisePoint point)
+        {
+            return point.VolumeValue;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -26,14 +54,10 @@ namespace BWGenerator
             Plot1.Controller = PlotsController;
             Plot2.Controller = PlotsController;
 
-            // creating some default preset
             Preset = new PresetModel();
-            Preset.Name = "Some name";
-            Preset.Description = "Some description";
-            // creating default signal with default params
-            Preset.Signals.Add(new PresetModel.Signal { Name = "Signal 1" });
-            Preset.Signals[0].SignalPoints.Add(new PresetModel.Signal.SignalPoint { Time = 0.0, DifferenceValue = 2.0, CarrierValue = 440.0 });
-            Preset.Signals[0].SignalPoints.Add(new PresetModel.Signal.SignalPoint { Time = 30.0, DifferenceValue = 10.0, CarrierValue = 440.0 });
+            Preset.Name = "New preset";
+            Preset.Description = "Write your description here";
+            Preset.Signals.Add(new Signal { Name = "Signal 1" });
 
             DataContext = Preset;
             playback = new Playback(new ModelledSampleProvider());
@@ -43,94 +67,47 @@ namespace BWGenerator
         public PlotController PlotsController { get; set; }
         private Playback playback = null;
 
-        private static PlotModel CreateCarrierModel()
+        private void SelectSignal(int signalId)
         {
-            var model = new PlotModel { LegendSymbolLength = 40 };
-            model.Axes.Add(new LinearAxis
+            if (signalId < 0 || Plot1Type.SelectedIndex < 0 || Plot2Type.SelectedIndex < 0)
+                return;
+
+            switch ((Graphs)Plot1Type.SelectedIndex)
             {
-                MajorGridlineStyle = LineStyle.Solid,
-                MinorGridlineStyle = LineStyle.Solid
-            });
-            model.Axes.Add(new LinearAxis
+                case Graphs.Carrier:
+                    models[0] = new SignalPlotViewModel(Preset.Signals[signalId], CarrierSelector);
+                    break;
+                case Graphs.Difference:
+                    models[0] = new SignalPlotViewModel(Preset.Signals[signalId], DifferenceSelector);
+                    break;
+                case Graphs.Volume:
+                    models[0] = new SignalPlotViewModel(Preset.Signals[signalId], VolumeSelector);
+                    break;
+                default:
+                    throw new InvalidCastException();
+            }
+
+            switch ((Graphs)Plot2Type.SelectedIndex)
             {
-                MajorGridlineStyle = LineStyle.Solid,
-                MinorGridlineStyle = LineStyle.Solid,
-                Position = AxisPosition.Bottom
-            });
+                case Graphs.Carrier:
+                    models[1] = new SignalPlotViewModel(Preset.Signals[signalId], CarrierSelector);
+                    break;
+                case Graphs.Difference:
+                    models[1] = new SignalPlotViewModel(Preset.Signals[signalId], DifferenceSelector);
+                    break;
+                case Graphs.Volume:
+                    models[1] = new SignalPlotViewModel(Preset.Signals[signalId], VolumeSelector);
+                    break;
+                default:
+                    throw new InvalidCastException();
+            }
+            Plot1.Model = models[0].model;
+            Plot2.Model = models[1].model;
 
-            // Add a line series
-            var s1 = new LineSeries
-            {
-                Color = OxyColors.SkyBlue,
-                MarkerType = MarkerType.Square,
-                MarkerSize = 6,
-                MarkerStroke = OxyColors.White,
-                MarkerFill = OxyColors.SkyBlue,
-                MarkerStrokeThickness = 1.5
-            };
-            s1.Points.Add(new DataPoint(0, 10));
-            s1.Points.Add(new DataPoint(10, 40));
-            s1.Points.Add(new DataPoint(40, 20));
-            s1.Points.Add(new DataPoint(60, 30));
-            model.Series.Add(s1);
-
-            int indexOfPointToMove = -1;
-
-            // Subscribe to the mouse down event on the line series
-            s1.MouseDown += (s, e) =>
-            {
-                // only handle the left mouse button (right button can still be used to pan)
-                if (e.ChangedButton != OxyMouseButton.Left)
-                    return;
-
-                int indexOfNearestPoint = (int)Math.Round(e.HitTestResult.Index);
-                var nearestPoint = s1.Transform(s1.Points[indexOfNearestPoint]);
-
-                // Check if we are near a point
-                if ((nearestPoint - e.Position).Length < 10)
-                {
-                    // Start editing this point
-                    indexOfPointToMove = indexOfNearestPoint;
-                }
-                else
-                {
-                    // otherwise create a point on the current line segment
-                    int i = (int)e.HitTestResult.Index + 1;
-                    s1.Points.Insert(i, s1.InverseTransform(e.Position));
-                    indexOfPointToMove = i;
-                }
-
-                // Remember to refresh/invalidate of the plot
-                model.InvalidatePlot(false);
-
-                // Set the event arguments to handled - no other handlers will be called.
-                e.Handled = true;
-            };
-
-            s1.MouseMove += (s, e) =>
-            {
-                if (indexOfPointToMove == -1)
-                    return;
-
-                // Move the point being edited.
-                s1.Points[indexOfPointToMove] = s1.InverseTransform(e.Position);
-                model.InvalidatePlot(false);
-                e.Handled = true;
-            };
-
-            s1.MouseUp += (s, e) =>
-            {
-                // Stop editing
-                indexOfPointToMove = -1;
-                s1.LineStyle = LineStyle.Solid;
-                model.InvalidatePlot(false);
-                e.Handled = true;
-            };
-            return model;
-        }
-
-        private void PresetSignalsComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
+            noiseSmoothnessModel = new NoisePlotViewModel(Preset, NoiseSmoothnessSelector);
+            noiseVolumeModel = new NoisePlotViewModel(Preset, NoiseVolumeSelector);
+            NoiseSmth.Model = noiseSmoothnessModel.model;
+            NoiseVol.Model = noiseVolumeModel.model;
         }
 
         private void Play_Click(object sender, RoutedEventArgs e)
@@ -150,11 +127,11 @@ namespace BWGenerator
 
         private void AddSignalButton_Click(object sender, RoutedEventArgs e)
         {
-            PresetModel.Signal lastSignal = Preset.Signals.Last();
+            Signal lastSignal = Preset.Signals.Last();
             string lastSignalName = lastSignal.Name;
             int lastIndex = int.Parse(lastSignalName.Substring(lastSignalName.Length - 1));
             lastIndex++;
-            Preset.Signals.Add(new PresetModel.Signal { Name = "Signal " + lastIndex.ToString() });
+            Preset.Signals.Add(new Signal { Name = "Signal " + lastIndex.ToString() });
 
             PresetSignalsComboBox.SelectedIndex = Preset.Signals.Count - 1;
         }
@@ -171,84 +148,68 @@ namespace BWGenerator
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            SignalPropertiesWindow sp = new SignalPropertiesWindow(Preset.Signals.First());
-            sp.ShowDialog();
         }
 
-        //private static PlotModel CreateDifferenceModel()
-        //{
-        //    //PlotBaseModel model = new PlotBaseModel();
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
 
-        //    //// Add a line series
-        //    //var s1 = new LineSeries
-        //    //{
-        //    //    Color = OxyColors.SkyBlue,
-        //    //    MarkerType = MarkerType.Square,
-        //    //    MarkerSize = 6,
-        //    //    MarkerStroke = OxyColors.White,
-        //    //    MarkerFill = OxyColors.SkyBlue,
-        //    //    MarkerStrokeThickness = 1.5
-        //    //};
-        //    //s1.Points.Add(new DataPoint(0.0, 5.0));
-        //    //s1.Points.Add(new DataPoint(10.0, 5.0));
-        //    //s1.Points.Add(new DataPoint(20.0, 7.0));
-        //    //s1.Points.Add(new DataPoint(30.0, 8.0));
-        //    //s1.Points.Add(new DataPoint(40.0, 10.0));
-        //    //model.Series.Add(s1);
+        private void PresetSignalsComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectSignal(PresetSignalsComboBox.SelectedIndex);
+        }
 
-        //    //int indexOfPointToMove = -1;
+        private void Plot1Type_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int selectedIndex = PresetSignalsComboBox.SelectedIndex;
+            if (selectedIndex < 0)
+                return;
 
-        //    //// Subscribe to the mouse down event on the line series
-        //    //s1.MouseDown += (s, e) =>
-        //    //{
-        //    //// only handle the left mouse button (right button can still be used to pan)
-        //    //if (e.ChangedButton != OxyMouseButton.Left)
-        //    //        return;
+            SignalPlotViewModel modelToCheck = models[0];
 
-        //    //    int indexOfNearestPoint = (int)Math.Round(e.HitTestResult.Index);
-        //    //    var nearestPoint = s1.Transform(s1.Points[indexOfNearestPoint]);
+            switch ((Graphs)Plot1Type.SelectedIndex)
+            {
+                case Graphs.Carrier:
+                    modelToCheck = new SignalPlotViewModel(Preset.Signals[selectedIndex], CarrierSelector);
+                    break;
+                case Graphs.Difference:
+                    modelToCheck = new SignalPlotViewModel(Preset.Signals[selectedIndex], DifferenceSelector);
+                    break;
+                case Graphs.Volume:
+                    modelToCheck = new SignalPlotViewModel(Preset.Signals[selectedIndex], VolumeSelector);
+                    break;
+                default:
+                    throw new InvalidCastException();
+            }
 
-        //    //// Check if we are near a point
-        //    //if ((nearestPoint - e.Position).Length < 10)
-        //    //    {
-        //    //    // Start editing this point
-        //    //    indexOfPointToMove = indexOfNearestPoint;
-        //    //    }
-        //    //    else
-        //    //    {
-        //    //    // otherwise create a point on the current line segment
-        //    //    int i = (int)e.HitTestResult.Index + 1;
-        //    //        s1.Points.Insert(i, s1.InverseTransform(e.Position));
-        //    //        indexOfPointToMove = i;
-        //    //    }
+            Plot1.Model = modelToCheck.model;
+        }
 
-        //    //// Remember to refresh/invalidate of the plot
-        //    //model.InvalidatePlot(false);
+        private void Plot2Type_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int selectedIndex = PresetSignalsComboBox.SelectedIndex;
+            if (selectedIndex < 0)
+                return;
 
-        //    //// Set the event arguments to handled - no other handlers will be called.
-        //    //e.Handled = true;
-        //    //};
+            SignalPlotViewModel modelToCheck = models[1];
 
-        //    //s1.MouseMove += (s, e) =>
-        //    //{
-        //    //    if (indexOfPointToMove == -1)
-        //    //        return;
+            switch ((Graphs)Plot2Type.SelectedIndex)
+            {
+                case Graphs.Carrier:
+                    modelToCheck = new SignalPlotViewModel(Preset.Signals[selectedIndex], CarrierSelector);
+                    break;
+                case Graphs.Difference:
+                    modelToCheck = new SignalPlotViewModel(Preset.Signals[selectedIndex], DifferenceSelector);
+                    break;
+                case Graphs.Volume:
+                    modelToCheck = new SignalPlotViewModel(Preset.Signals[selectedIndex], VolumeSelector);
+                    break;
+                default:
+                    throw new InvalidCastException();
+            }
 
-        //    //// Move the point being edited.
-        //    //s1.Points[indexOfPointToMove] = s1.InverseTransform(e.Position);
-        //    //    model.InvalidatePlot(false);
-        //    //    e.Handled = true;
-        //    //};
-
-        //    //s1.MouseUp += (s, e) =>
-        //    //{
-        //    //// Stop editing
-        //    //indexOfPointToMove = -1;
-        //    //    s1.LineStyle = LineStyle.Solid;
-        //    //    model.InvalidatePlot(false);
-        //    //    e.Handled = true;
-        //    //};
-        //    //return model;
-        //}
+            Plot2.Model = modelToCheck.model;
+        }
     }
 }
