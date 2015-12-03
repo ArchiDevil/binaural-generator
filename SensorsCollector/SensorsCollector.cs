9c02045a63602ad4d35465906be9d790adb40dec
ArchiDevil <archidevil52@gmail.com>
2015-12-03 19:00:19 +0300

    Added more interface methods as draft

--- SensorsCollector/SensorsCollector.cs
+++ SensorsCollector/SensorsCollector.cs
@@ -13,13 +13,27 @@ namespace SensorsLayer
         {
         }
 
-        public List<string> GetAllPorts()
+        public bool FindDevice()
+        {
+            return false;
+        }
+
+        private List<string> GetAllPorts()
         {
             List<string> allPorts = new List<string>();
             foreach (string portName in SerialPort.GetPortNames())
                 allPorts.Add(portName);
 
             return allPorts;
+        }
+
+        private void SelectPort(string portName)
+        {
+            SerialPort myPort = new SerialPort(portName);
+            if (!myPort.IsOpen)
+                myPort.Open();
+
+            myPort.Close();
         }
 
         private void CollectCurrentValues()
