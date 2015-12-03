22b013d2fc587bf25632fb0c1553380caac941d0
ArchiDevil <archidevil52@gmail.com>
2015-12-03 18:54:25 +0300

    Added initial sample that enumerates COM ports in the system

---
+++ SensorsCollector/SensorsCollector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorsLayer
+{
+    public class SensorsCollector
+    {
+        public SensorsCollector()
+        {
+        }
+
+        public List<string> GetAllPorts()
+        {
+            List<string> allPorts = new List<string>();
+            foreach (string portName in SerialPort.GetPortNames())
+                allPorts.Add(portName);
+
+            return allPorts;
+        }
+
+        private void CollectCurrentValues()
+        {
+
+        }
+    }
+}
---
+++ SerialPortApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SensorsLayer;
+
+namespace SerialPortApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SensorsCollector collector = new SensorsCollector();
+            List<string> ports = collector.GetAllPorts();
+            foreach(var port in ports)
+                Console.WriteLine(port);
+
+
+        }
+    }
+}
