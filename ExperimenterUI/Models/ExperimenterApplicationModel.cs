4bf672da6b08f152511f56a68d9db2df1e302bef
ArchiDevil <archidevil52@gmail.com>
2015-12-08 19:40:22 +0300

    Added some session info

--- ExperimenterUI/Models/ExperimenterApplicationModel.cs
+++ ExperimenterUI/Models/ExperimenterApplicationModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 
 using ExperimenterUI.Models;
@@ -28,6 +29,8 @@ namespace ExperimenterUI
         private ClientProtocol protocol = null;
         private NoiseViewModel noiseModel = null;
         private SignalViewModel[] signalModels = null;
+        private TimeSpan _sessionTime = new TimeSpan(0, 0, 0);
+        private Timer _tickTimer = null;
 
         private void ChatMessageReceiveHandler(object sender, ClientChatMessageEventArgs e)
         {
@@ -49,7 +52,13 @@ namespace ExperimenterUI
             get { return _connectionStatus == ConnectionStatus.Connected; }
         }
 
-        public event ClientProtocol.ChatMessageReceiveHandler ChatMessageReceived = delegate { };
+        public TimeSpan SessionTime
+        {
+            get { return _sessionTime; }
+        }
+
+        public event ClientProtocol.ChatMessageReceiveHandler ChatMessageReceived = delegate
+        { };
 
         public ExperimenterApplicationModel(ClientProtocol protocol)
         {
@@ -68,6 +77,20 @@ namespace ExperimenterUI
             this.protocol = protocol;
             protocol.ChatMessageReceive += ChatMessageReceiveHandler;
             noiseModel.PropertyChanged += NoiseModelPropertyChanged;
+
+            _tickTimer = new Timer(1000);
+            _tickTimer.Elapsed += _tickTimer_Elapsed;
+            _tickTimer.AutoReset = true;
+            _tickTimer.Start();
+        }
+
+        private void _tickTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (_connectionStatus == ConnectionStatus.Connected)
+            {
+                _sessionTime += new TimeSpan(0, 0, 1);
+                RaisePropertyChanged("SessionTime");
+            }
         }
 
         private void SendNewSettings()
@@ -122,7 +145,7 @@ namespace ExperimenterUI
                     RaisePropertyChanged("IsConnected");
                 }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 MessageBox.Show(e.Message);
                 _connectionErrorMessage = e.Message;
