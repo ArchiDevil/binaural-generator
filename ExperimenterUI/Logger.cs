aeaf2180c049d240a7ff066f3b43c6492a52897f
ArchiDevil <archidevil52@gmail.com>
2016-01-09 03:49:08 +0300

    Resolved remain methods

--- ExperimenterUI/Logger.cs
+++ ExperimenterUI/Logger.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExperimenterUI.Models;
 
 namespace ExperimenterUI
 {
+    public class SensorsData
+    {
+        public double temperatureValue = 0.0;
+        public double skinResistanceValue = 0.0;
+        public double motionValue = 0.0;
+        public double pulseValue = 0.0;
+    }
+
     public class Logger
     {
         private DateTime sessionStart;
@@ -13,6 +22,9 @@ namespace ExperimenterUI
         private bool sessionStarted = false;
         private Dictionary<int, byte[]> experimenterVoiceData = new Dictionary<int, byte[]>();
         private Dictionary<int, byte[]> subjectVoiceData = new Dictionary<int, byte[]>();
+        private Dictionary<int, SensorsData> sensorsData = new Dictionary<int, SensorsData>();
+        private Dictionary<int, SignalViewModel[]> signalsData = new Dictionary<int, SignalViewModel[]>();
+        private Dictionary<int, NoiseViewModel> noiseData = new Dictionary<int, NoiseViewModel>();
 
         public void StartSession()
         {
@@ -26,14 +38,25 @@ namespace ExperimenterUI
             sessionEnd = DateTime.Now;
         }
 
-        public void LogSensors()
+        public void LogSensors(SensorsData sensorsData)
         {
             // log sensors data
+            if (!sessionStarted)
+                return;
+
+            DateTime eventTime = DateTime.Now;
+            TimeSpan delta = eventTime - sessionStart;
+            this.sensorsData.Add(delta.Seconds, sensorsData);
         }
 
-        public void LogSubjectVoice()
+        public void LogSubjectVoice(byte[] buffer)
         {
-            // log subject voice
+            if (!sessionStarted)
+                return;
+
+            DateTime eventTime = DateTime.Now;
+            TimeSpan delta = eventTime - sessionStart;
+            subjectVoiceData.Add(delta.Seconds, buffer);
         }
 
         public void LogExperimenterVoice(byte[] buffer)
@@ -46,19 +69,19 @@ namespace ExperimenterUI
             experimenterVoiceData.Add(delta.Seconds, buffer);
         }
 
-        public void LogSignalsChange(byte[] buffer)
+        public void LogSignalsChange(SignalViewModel[] signals, NoiseViewModel noise)
         {
             if (!sessionStarted)
                 return;
 
             DateTime eventTime = DateTime.Now;
             TimeSpan delta = eventTime - sessionStart;
-            subjectVoiceData.Add(delta.Seconds, buffer);
+            signalsData.Add(delta.Seconds, signals);
+            noiseData.Add(delta.Seconds, noise);
         }
 
         public void DumpData(string filename)
         {
-
         }
     }
 }
