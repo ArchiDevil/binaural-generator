93a7621529f0c550c98232689293eb26b2522583
ArchiDevil <archidevil52@gmail.com>
2016-01-18 22:52:35 +0300

    Code style

--- ExperimenterUI/Logger.cs
+++ ExperimenterUI/Logger.cs
@@ -17,67 +17,67 @@ namespace ExperimenterUI
 
     public class Logger
     {
-        private DateTime sessionStart;
-        private DateTime sessionEnd;
-        private bool sessionStarted = false;
-        private Dictionary<int, byte[]> experimenterVoiceData = new Dictionary<int, byte[]>();
-        private Dictionary<int, byte[]> subjectVoiceData = new Dictionary<int, byte[]>();
-        private Dictionary<int, SensorsData> sensorsData = new Dictionary<int, SensorsData>();
-        private Dictionary<int, SignalViewModel[]> signalsData = new Dictionary<int, SignalViewModel[]>();
-        private Dictionary<int, NoiseViewModel> noiseData = new Dictionary<int, NoiseViewModel>();
+        private DateTime _sessionStart;
+        private DateTime _sessionEnd;
+        private bool _sessionStarted = false;
+        private Dictionary<int, byte[]> _experimenterVoiceData = new Dictionary<int, byte[]>();
+        private Dictionary<int, byte[]> _subjectVoiceData = new Dictionary<int, byte[]>();
+        private Dictionary<int, SensorsData> _sensorsData = new Dictionary<int, SensorsData>();
+        private Dictionary<int, SignalViewModel[]> _signalsData = new Dictionary<int, SignalViewModel[]>();
+        private Dictionary<int, NoiseViewModel> _noiseData = new Dictionary<int, NoiseViewModel>();
 
         public void StartSession()
         {
-            sessionStart = DateTime.Now;
-            sessionStarted = true;
+            _sessionStart = DateTime.Now;
+            _sessionStarted = true;
         }
 
         public void EndSession()
         {
-            sessionStarted = false;
-            sessionEnd = DateTime.Now;
+            _sessionStarted = false;
+            _sessionEnd = DateTime.Now;
         }
 
         public void LogSensors(SensorsData sensorsData)
         {
             // log sensors data
-            if (!sessionStarted)
+            if (!_sessionStarted)
                 return;
 
             DateTime eventTime = DateTime.Now;
-            TimeSpan delta = eventTime - sessionStart;
-            this.sensorsData.Add(delta.Seconds, sensorsData);
+            TimeSpan delta = eventTime - _sessionStart;
+            this._sensorsData.Add(delta.Seconds, sensorsData);
         }
 
         public void LogSubjectVoice(byte[] buffer)
         {
-            if (!sessionStarted)
+            if (!_sessionStarted)
                 return;
 
             DateTime eventTime = DateTime.Now;
-            TimeSpan delta = eventTime - sessionStart;
-            subjectVoiceData.Add(delta.Seconds, buffer);
+            TimeSpan delta = eventTime - _sessionStart;
+            _subjectVoiceData.Add(delta.Seconds, buffer);
         }
 
         public void LogExperimenterVoice(byte[] buffer)
         {
-            if (!sessionStarted)
+            if (!_sessionStarted)
                 return;
 
             DateTime eventTime = DateTime.Now;
-            TimeSpan delta = eventTime - sessionStart;
-            experimenterVoiceData.Add(delta.Seconds, buffer);
+            TimeSpan delta = eventTime - _sessionStart;
+            _experimenterVoiceData.Add(delta.Seconds, buffer);
         }
 
         public void LogSignalsChange(SignalViewModel[] signals, NoiseViewModel noise)
         {
-            if (!sessionStarted)
+            if (!_sessionStarted)
                 return;
 
             DateTime eventTime = DateTime.Now;
-            TimeSpan delta = eventTime - sessionStart;
-            signalsData.Add(delta.Seconds, signals);
-            noiseData.Add(delta.Seconds, noise);
+            TimeSpan delta = eventTime - _sessionStart;
+            _signalsData.Add(delta.Seconds, signals);
+            _noiseData.Add(delta.Seconds, noise);
         }
 
         public void DumpData(string filename)
